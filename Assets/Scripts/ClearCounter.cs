79a91d659d424fd41e109a9d2d94e2094d99ba78
Sarvottam <sarvottamswaroop100@gmail.com>
2023-10-01 00:38:25 +0530

    feat(Prefabs,Player,ClearCounter): Added prefab variants for all kitchenobjects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,6 +9,29 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //player has nothing
+            }
+        }
+        else
+        {
+            //we already have a kitchen object 
+            if (player.HasKitchenObject())
+            {
+                //player is carrying something
+            }
+            else
+            {
+                //player is not carrying anythin
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
